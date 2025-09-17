608e3447512922dab39637f50396e2b30656cecf
gato1706 <thiago.gato17g@gmail.com>
2025-09-16 21:48:05 -0300

    teste

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,21 @@
-import { calcularSaldo, classificarSaldo } from "./calculoSaldo"
+import { despesas } from "./state.js"
 
-const form = document.getElementById('formIMC')
-const resultado = document.getElementById('resultado')
+document.addEventListener("DOMContentLoaded", () => {
+  const btnAdicionar = document.getElementById("btnAdicionar")
 
-//listener no submit do from
-form.addEventListener(' submit', (e) => {
-    e.preventDefault() //evita o recarregamennto do form
-    const receita = document.getElementById('receita').value
-    const despesas = document.getElementById('despesas').value
-    try {
-        const saldo = calcularSaldo(receita, despesas)
-        //desestruturação de objeto - object desustrucuting
-        const {texto, classe} = classificarSaldo(saldo)
-        resultado.textContent  = `SALDO: ${saldo.toFixed(2)} - ${texto}`
-        resultado.className = classe
-        
-    } catch {
-        resultado.textContent = `❌Erro: ${error.message}`
-        resultado.className = 'resultado-erro'
+  btnAdicionar.addEventListener("click", () => {
+    let valor = parseFloat(document.getElementById("valorDespesa").value)
+    let categoria = document.getElementById("categoria").value
+
+    if (isNaN(valor) || valor <= 0) {
+      alert("Valor da despesa deve ser maior que 0")
+      return
     }
-})
 
-form.addEventListener('reset', () =>{
-    //limpando o conteúdo do resultado
-    resultado.textContent = ''
-    resultado.className = ''
+    const novaDespesa = { valor, categoria }
+    despesas.push(novaDespesa)
+
+    console.log("Despesa adicionada:", novaDespesa)
+    console.log("Todas as despesas:", despesas)
+  })
 })
