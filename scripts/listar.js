c16e806486f73265db5578ebfae824caee9e244f
gato1706 <thiago.gato17g@gmail.com>
2025-10-02 14:53:12 -0300

    criar função de listar as despesas criando o elemento html

---
+++ scripts/listar.js
@@ -0,0 +1,24 @@
+import { getDespesas } from "./storage.js";
+
+
+export function listarDespesas(){
+  const tabela = document.getElementById("tabelaDespesas");
+
+  if (tabela) {
+    let despesas = getDespesas();
+
+    despesas.forEach((d) => {
+      let linha = document.createElement("tr");
+
+      linha.innerHTML = `
+        <td>R$ ${d.valor}</td>
+        <td>${d.categoria}</td>
+        <td>${d.descricao}</td>
+        <td>${d.data}</td>
+      `;
+
+      tabela.appendChild(linha);
+    });
+  }
+
+}
