8aa6544fda01790aa04c5d0e396c679b3de0d301
gato1706 <thiago.gato17g@gmail.com>
2025-10-02 15:15:09 -0300

    criar aquivo main que contém as funções em módulos

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { adicionarDespesa } from "./adicionarDespesa.js";
+import { listarDespesas } from "./listar.js";
+
+document.addEventListener("DOMContentLoaded", () =>{
+    adicionarDespesa()
+    listarDespesas()
+})
