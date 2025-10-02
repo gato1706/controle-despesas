96d3c5ea59926ce7e4f75998e3a4a7a01faf7795
gato1706 <thiago.gato17g@gmail.com>
2025-10-02 15:19:14 -0300

    criar logica de pegar as despesas do localstorage e de salvar as despesas no localstorage

---
+++ scripts/storage.js
@@ -0,0 +1,8 @@
+// Funções de salvar e recuperar despesas
+export function getDespesas() {
+  return JSON.parse(localStorage.getItem("despesas")) || [];
+}
+
+export function salvarDespesas(despesas) {
+  localStorage.setItem("despesas", JSON.stringify(despesas));
+}
