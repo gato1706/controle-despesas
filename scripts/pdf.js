dcee329f0e718aae3664067b8e79b6e4c67993b4
GabrielMonteirogms <gabrielmontsilva16@gmail.com>
2025-10-06 20:46:46 -0300

    documentar todos os modulos e funcoes js em JSDOC

--- scripts/pdf.js
+++ scripts/pdf.js
@@ -1,3 +1,30 @@
+/**
+ * @author Gabriel Monteiro <gabrielmontsilva16@gmail.com>
+ * @author Vitor Cruz <provitor39@gmail.com>
+ * @author Thiago Gato <thiago.gato17g@gmail.com>
+ * @version 1.0.0
+ * @since 1.0.0 (05/10/2025)
+ * 
+ * @file exportarPDF.js
+ * @description
+ * Este módulo é responsável por gerar e exportar o relatório de despesas em formato PDF.
+ * 
+ * Ao clicar no botão com o ID `#btnExportar`, o conteúdo do elemento `#pdf-content`
+ * é capturado e convertido em um arquivo PDF utilizando a biblioteca **html2pdf.js**.
+ *
+ * O arquivo gerado é salvo automaticamente no dispositivo do usuário.
+ *
+ * @requires html2pdf.js
+ *
+ * @example
+ * // Estrutura HTML esperada:
+ * // <div id="pdf-content">
+ * //   <!-- conteúdo do relatório -->
+ * // </div>
+ * // <button id="btnExportar">Exportar PDF</button>
+ *
+ * // Ao clicar no botão, o relatório será baixado como "relatorio_despesas.pdf" exportando o relatório mensal das despesas, caso o usuário queira de forma física.
+ */
 const btnGenerate = document.querySelector("#btnExportar");
 btnGenerate.addEventListener("click", () => {
     const content = document.querySelector("#pdf-content");
